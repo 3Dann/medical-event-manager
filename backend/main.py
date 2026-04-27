d9c94a339e1b5f3eb808f0831b78355f4c592a8f
3Dann <da.tzalik@gmail.com>
2026-04-27 13:49:22 +0300

    auto: 2026-04-27 13:49

--- backend/main.py
+++ backend/main.py
@@ -473,6 +473,7 @@ app.include_router(workflows.router)
 app.include_router(webauthn_routes.router)
 app.include_router(specialties.router)
 app.include_router(medications_routes.router)
+app.include_router(policy_ai_routes.router)
 
 
 def _seed_step_task_templates(db, step_template, tasks):
