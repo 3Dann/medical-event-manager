c02e1a5f1a037220c1257e354b1f44398d1de43b
3Dann <da.tzalik@gmail.com>
2026-04-30 15:43:13 +0300

    auto: 2026-04-30 15:43

--- backend/routes/claims.py
+++ backend/routes/claims.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy import select
 from pydantic import BaseModel
 from typing import Optional
 from database import get_db
