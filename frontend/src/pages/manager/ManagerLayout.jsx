8fdcc023c6b969ab6d14b23b7b894d6447fe0edf
3Dann <da.tzalik@gmail.com>
2026-05-05 20:28:45 +0300

    auto: 2026-05-05 20:28

--- frontend/src/pages/manager/ManagerLayout.jsx
+++ frontend/src/pages/manager/ManagerLayout.jsx
@@ -275,7 +275,7 @@ export default function ManagerLayout() {
           Orly Medical
         </div>
 
-        <div className="flex-1 relative z-10">
+        <div className="flex-1 relative z-10 flex flex-col min-h-0">
           <Outlet />
         </div>
 
