f51064e95fcd20e78189c26becbb17b441eda78f
3Dann <da.tzalik@gmail.com>
2026-05-09 08:24:24 +0300

    auto: 2026-05-09 08:24

--- frontend/src/pages/manager/ManagerLayout.jsx
+++ frontend/src/pages/manager/ManagerLayout.jsx
@@ -14,7 +14,8 @@ const navItems = [
   { to: '/manager/reports', tKey: 'nav:reports', icon: 'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z' },
   { to: '/manager/profile', tKey: 'nav:profile', icon: 'M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0z' },
 ]
-const adminNavItem        = { to: '/manager/admin',          tKey: 'nav:admin',          icon: 'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z' }
+const adminNavItem        = { to: '/manager/admin',           tKey: 'nav:admin',            icon: 'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z' }
+const adminDashboardNavItem = { to: '/manager/admin-dashboard', tKey: 'nav:admin_dashboard',  icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' }
 const landingEditorNavItem = { to: '/manager/landing-editor', tKey: 'nav:landing_editor', icon: 'M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z' }
 const feedbackInboxNavItem = { to: '/manager/feedback',       tKey: 'nav:feedback',       icon: 'M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-3 3v-3z' }
 
