9eb47a8577b6dda6867f2a34ee3f46e0bf325ff8
3Dann <da.tzalik@gmail.com>
2026-05-10 14:57:51 +0300

    auto: 2026-05-10 14:57 [skip ci]

--- backend/main.py
+++ backend/main.py
@@ -328,6 +328,7 @@ app.include_router(form17_routes.router)
 app.include_router(red_flags_routes.router)
 app.include_router(reports_routes.router)
 app.include_router(patient_portal_routes.router)
+app.include_router(family_share_routes.router)
 
 
 def _seed_step_task_templates(db, step_template, tasks):
