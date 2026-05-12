80f543412bbddaf1e99cfb5138920d393807e995
3Dann <da.tzalik@gmail.com>
2026-05-12 17:16:12 +0300

    auto: 2026-05-12 17:16 [skip ci]

--- frontend/src/pages/manager/DoctorsDatabase.jsx
+++ frontend/src/pages/manager/DoctorsDatabase.jsx
@@ -559,7 +559,7 @@ export default function DoctorsDatabase() {
             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4 4l4-4m0 0l4-4m-4 4V4" />
             </svg>
-            ייבוא Excel
+            {t('import_excel')}
           </button>
           <button
             onClick={() => setShowColPicker(v => !v)}
