3b588ae727fffcc07fc5fd4241a5710bee24c2ae
3Dann <da.tzalik@gmail.com>
2026-05-12 11:33:00 +0300

    auto: 2026-05-12 11:33 [skip ci]

--- backend/main.py
+++ backend/main.py
@@ -143,8 +143,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=_cors_origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 app.add_middleware(AuditMiddleware)
