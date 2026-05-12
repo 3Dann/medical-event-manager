232262b8457b31aca7296ef1a5500af8070770fd
3Dann <da.tzalik@gmail.com>
2026-05-12 15:59:10 +0300

    auto: 2026-05-12 15:59 [skip ci]

--- frontend/src/pages/manager/PatientMeetings.jsx
+++ frontend/src/pages/manager/PatientMeetings.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from 'react'
+import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { fmtDate } from '../../utils/formatters'
