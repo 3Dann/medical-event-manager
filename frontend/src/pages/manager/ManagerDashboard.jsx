ffe7c639358be34fcd189ae45b029156e3e29339
3Dann <da.tzalik@gmail.com>
2026-05-12 14:52:24 +0300

    auto: 2026-05-12 14:52 [skip ci]

--- frontend/src/pages/manager/ManagerDashboard.jsx
+++ frontend/src/pages/manager/ManagerDashboard.jsx
@@ -181,7 +181,7 @@ export default function ManagerDashboard() {
                   </button>
                 </div>
               </div>
-            </div>
+            </button>
           ))}
         </div>
       )}
