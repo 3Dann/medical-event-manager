dcb307b929d584f21d5633d656fbe3b7f9a87cc9
3Dann <da.tzalik@gmail.com>
2026-05-14 13:08:17 +0300

    auto: 2026-05-14 13:08 [skip ci]

--- frontend/src/components/PasskeySection.jsx
+++ frontend/src/components/PasskeySection.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { fmtDate } from '../utils/formatters'
+import { useConfirm } from './ConfirmDialog'
 import {
   startRegistration,
   browserSupportsWebAuthn,
